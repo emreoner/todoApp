5b768afcd22d1e99fd4b0c5eb1ce220dba92f340
PROIST\emre.oner <emre.oner@pro-line.com.tr>
2020-09-07 11:44:58 +0300

    d

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoApp';
+  user = 'Emress';
+  items=[
+    {description:"Kahvaltı",action:"No"},
+    {description:"Sinema",action:"No"},
+    {description:"Spor",action:"No"},
+    {description:"Ders",action:"No"}
+  ];
 }
